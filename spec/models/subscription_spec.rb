2b66ebfbdab58adab7242b26fc7eaa3b42ec241b
Alicia Watt <alicia.watt.aw@gmail.com>
2022-07-27 18:31:04 -0600

    test: write model tests for customer, subscription, and tea

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,12 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:customer) }
+  it { should belong_to(:tea) }
+
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:price) }
+  it { should validate_numericality_of(:price) }
+  it { should validate_presence_of(:status) }
+  it { should validate_presence_of(:frequency) }
 end
