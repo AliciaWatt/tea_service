fde6c53341bec137a9ab89d00c2b1e9ea5e470dc
Alicia Watt <alicia.watt.aw@gmail.com>
2022-07-28 12:04:47 -0600

    seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Customer.create!(first_name: "Tia", last_name: "Sipper", email: "example@email.com", address: "221 B Baker St.")
+
+Tea.create!(title: "SleepyTime", description: "A delightful blend of chamomile and other herbs to put you to bed", temperature: 180, brew_time: 300)
+Tea.create!(title: "Mint Majesty", description: "All the mint none of the caffeine", temperature: 150, brew_time: 360)
+Tea.create!(title: "Earl Grey", description: "perfect for making a london fog", temperature: 150, brew_time: 360)
+
+Subscription.create!(customer_id: 1, tea_id: 1, title: "SleepyTime", price: 399, status: "subscribed", frequency: 1)
+Subscription.create!(customer_id: 1, tea_id: 3, title: "Earl Grey", price: 399, status: "subscribed", frequency: 1)
+Subscription.create!(customer_id: 1, tea_id: 2, title: "Mint Majesty", price: 399, status: "unsubscribed", frequency: 1)
