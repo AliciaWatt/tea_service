b115d2392b1880e5f8734e764303518926c6be61
Alicia Watt <alicia.watt.aw@gmail.com>
2022-07-27 20:16:08 -0600

    test: returns all subscriptions for a customer

--- spec/requests/api/v1/subscriptions_request_spec.rb
+++ spec/requests/api/v1/subscriptions_request_spec.rb
@@ -31,4 +31,20 @@ RSpec.describe "subscriptions" do
     expect(response).to be_successful
     expect(updated_subscription.status).to eq("unsubscribed")
   end
+  it "has all subscriptions for a customer" do
+    customer_1 = Customer.create!(first_name: "Tia", last_name: "Sipper", email: "example@email.com", address: "221 B Baker St.")
+    customer_2 = Customer.create!(first_name: "Earl", last_name: "Grey", email: "any@email.com", address: "101 Grove St")
+    tea_1 = Tea.create!(title: "SleepyTime", description: "A delightful blend of chamomile and other herbs to put you to bed", temperature: 180, brew_time: 300)
+    tea_2 = Tea.create!(title: "Mint Majesty", description: "All the mint none of the caffeine", temperature: 150, brew_time: 360)
+    subscription_1 = Subscription.create!(customer_id: customer_1.id, tea_id: tea_1.id, title: "Title", price: 399, status: "subscribed", frequency: 1)
+    subscription_2 = Subscription.create!(customer_id: customer_1.id, tea_id: tea_2.id, title: "Title", price: 399, status: "unsubscribed", frequency: 1)
+    subscription_3 = Subscription.create!(customer_id: customer_2.id, tea_id: tea_1.id, title: "Title", price: 399, status: "subscribed", frequency: 1)
+    subscription_4 = Subscription.create!(customer_id: customer_2.id, tea_id: tea_2.id, title: "Title", price: 399, status: "unsubscribed", frequency: 1)
+
+    get "/api/v1/subscriptions", params: {customer_id: customer_1.id}
+    expect(response).to be_successful
+    subscriptions = JSON.parse(response.body, symbolize_names: true)
+
+    expect(subscriptions).to be_a(Hash)
+  end
 end
